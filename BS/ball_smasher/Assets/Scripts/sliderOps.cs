cb6387bac33fd0231dca9bf602fba7bc0cbd4f36
vismay2711 <visu.patel11@gmail.com>
2019-09-12 16:58:15 +0530

    slider script is created lose collision trigger is configurred game over scene is created bouncing physics is created for the ball

---
+++ BS/ball_smasher/Assets/Scripts/sliderOps.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sliderOps : MonoBehaviour
+{
+    [SerializeField] float screenWInUnits = 16f;
+    [SerializeField] float minScreenX = 1.3f;
+    [SerializeField] float maxScreenX = 14.7f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWInUnits;
+
+        Vector2 sliderPos = new Vector2(transform.position.x , transform.position.y);
+        sliderPos.x = Mathf.Clamp(mousePosInUnits, minScreenX, maxScreenX);
+        transform.position = sliderPos;
+    }
+}
+
+// notes
+
+// Debug.Log("mouse Position " + Input.mousePosition + " X : " + Input.mousePosition.x + " Y : " + Input.mousePosition.y + " Z : " + Input.mousePosition.z);
+// Debug.Log("Screen Width : " + Screen.width + " Height : " + Screen.height);
+//Debug.Log(Input.mousePosition.x / Screen.width * screenWInUnits); // mouse mosition in units with screen aspect ration
